e4a35299d81367ce7051e01876872d2cc453601a
Simone Gotti <simone.gotti@gmail.com>
2018-08-23 11:29:43 +0200

    keeper: remove trailing new lines from passwords
    Remove trailing new lines (trailing line feed and carriage return chars) like
    already done by initdb and libpq when reading a password file.

--- tests/integration/init_test.go
+++ tests/integration/init_test.go
@@ -512,3 +512,90 @@ func TestExclusiveLock(t *testing.T) {
 		t.Fatalf("expecting keeper reporting that failed to take an exclusive lock on data dir")
 	}
 }
+
+func TestPasswordTrailingNewLine(t *testing.T) {
+	t.Parallel()
+
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	tstore, err := NewTestStore(t, dir)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if err := tstore.Start(); err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if err := tstore.WaitUp(10 * time.Second); err != nil {
+		t.Fatalf("error waiting on store up: %v", err)
+	}
+	storeEndpoints := fmt.Sprintf("%s:%s", tstore.listenAddress, tstore.port)
+	defer tstore.Stop()
+
+	clusterName := uuid.NewV4().String()
+
+	u := uuid.NewV4()
+	id := fmt.Sprintf("%x", u[:4])
+
+	pgSUPassword := "stolon_superuserpassword\n"
+	pgReplPassword := "stolon_replpassword\n"
+
+	tk, err := NewTestKeeperWithID(t, dir, id, clusterName, pgSUUsername, pgSUPassword, pgReplUsername, pgReplPassword, tstore.storeBackend, storeEndpoints)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if err := tk.StartExpect(); err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	if err := tk.cmd.ExpectTimeout("superuser password contain trailing new line, removing", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "superuser password contain trailing new line, removing"`)
+	}
+	if err := tk.cmd.ExpectTimeout("replication user password contain trailing new line, removing", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "replication user password contain trailing new line, removing"`)
+	}
+	tk.Stop()
+
+	pgSUPassword = "stolon_superuserpassword\n"
+	pgReplPassword = "\n"
+
+	tk, err = NewTestKeeperWithID(t, dir, id, clusterName, pgSUUsername, pgSUPassword, pgReplUsername, pgReplPassword, tstore.storeBackend, storeEndpoints)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if err := tk.StartExpect(); err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	if err := tk.cmd.ExpectTimeout("replication user password contain trailing new line, removing", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "replication user password contain trailing new line, removing"`)
+	}
+	if err := tk.cmd.ExpectTimeout("replication user password is empty after removing trailing new line", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "replication user password is empty after removing trailing new line"`)
+	}
+
+	tk.Stop()
+
+	pgSUPassword = "\n"
+	pgReplPassword = "stolon_replpassword\n"
+
+	tk, err = NewTestKeeperWithID(t, dir, id, clusterName, pgSUUsername, pgSUPassword, pgReplUsername, pgReplPassword, tstore.storeBackend, storeEndpoints)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if err := tk.StartExpect(); err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	if err := tk.cmd.ExpectTimeout("superuser password contain trailing new line, removing", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "superuser password contain trailing new line, removing"`)
+	}
+	if err := tk.cmd.ExpectTimeout("superuser password is empty after removing trailing new line", 30*time.Second); err != nil {
+		t.Fatalf(`expecting keeper reporting "superuser password is empty after removing trailing new line"`)
+	}
+
+	tk.Stop()
+}
